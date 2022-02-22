ac065325bcd0b5a48cf5e6ad5558abc5316f8eee
shiva <97592150+shiva8504@users.noreply.github.com>
2022-02-22 15:29:31 +0530

    #include<stdio.h> int main() {     int n,v,k;     scanf("%d",&n);     for(v=1;v<=n;v++)     {         for(k=1;k<=v;k++)         {             printf("%d",k);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,n;
+    int n,v,k;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    for(v=1;v<=n;v++)
     {
-        for(j=1;j<=n-i+1;j++)
-    {
-        printf("%d",j);
-    }
-    printf("
+        for(k=1;k<=v;k++)
+        {
+            printf("%d",k);
+        }
+        printf("
 ");
     }
-    return 0;
 }
