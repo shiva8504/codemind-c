5ab2325590a0117edda06b3ea279d5d95995c978
shiva <97592150+shiva8504@users.noreply.github.com>
2022-04-27 14:03:27 +0530

    #include<stdio.h> int main() {     int r,v,k;     scanf("%d",&r);     for(v=1;v<=r;v++)     {         for(k=r;k>=1;k--)         {             printf("%d ",v);         }         printf(" ");     } }

---
+++ Pattern.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int r,v,k;
+    scanf("%d",&r);
+    for(v=1;v<=r;v++)
+    {
+        for(k=r;k>=1;k--)
+        {
+            printf("%d ",v);
+        }
+        printf("
+");
+    }
+}
