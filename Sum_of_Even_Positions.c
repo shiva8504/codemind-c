ab4180b330c4b8897f77ef2fe23863882a000c39
shiva <97592150+shiva8504@users.noreply.github.com>
2022-05-19 11:27:19 +0530

    #include<stdio.h> int main() { int n,k,v[900],r=0; scanf("%d",&n); for(k=0;k<n;k++) {     scanf("%d",&v[k]); } for(k=n-1;k>=0;k--) { if(k%2==0) {  r+=v[k]; } } printf("%d",r); }

--- Sum_of_Even_Positions.c
+++ Sum_of_Even_Positions.c
@@ -1,19 +1,18 @@
 #include<stdio.h>
 int main()
 {
- int n,k,v[900],r=0;
- scanf("%d",&n);
- for(k=0;k<n;k++)
- {
- 
-   scanf("%d",&v[k]);
- }
-  for(k=n-1;k>=0;k--)
- {
- if(k%2==0)
- {
-   r+=v[k];
- }
- }
- printf("%d",r);
+int n,k,v[900],r=0;
+scanf("%d",&n);
+for(k=0;k<n;k++)
+{
+    scanf("%d",&v[k]);
+}
+for(k=n-1;k>=0;k--)
+{
+if(k%2==0)
+{
+ r+=v[k];
+}
+}
+printf("%d",r);
 }
