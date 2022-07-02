814b242bdd00a93d697008c766f257890346e799
shiva <97592150+shiva8504@users.noreply.github.com>
2022-07-02 20:51:25 +0530

    #include<stdio.h> #include<math.h> int main() {     int v[2004],r,k,n=0,l=0,j;     float m;     scanf("%d",&r);     for(k=0;k<r;k++)     {         scanf("%d",&v[k]);     }     for(k=0;k<r;k++)     {         for(j=1;j<=v[k];j++)         {             if(v[k]%j==0)             {                 n++;             }         }         if(n==2)         {             l++;         }         n=0;     }     printf("%d",l); }

---
+++ Primes_in_the_array.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int v[2004],r,k,n=0,l=0,j;
+    float m;
+    scanf("%d",&r);
+    for(k=0;k<r;k++)
+    {
+        scanf("%d",&v[k]);
+    }
+    for(k=0;k<r;k++)
+    {
+        for(j=1;j<=v[k];j++)
+        {
+            if(v[k]%j==0)
+            {
+                n++;
+            }
+        }
+        if(n==2)
+        {
+            l++;
+        }
+        n=0;
+    }
+    printf("%d",l);
+}
