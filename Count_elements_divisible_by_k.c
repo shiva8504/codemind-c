80693553e005cfcdf88e322c42f4d86ed89b17e7
shiva <97592150+shiva8504@users.noreply.github.com>
2022-07-02 20:46:09 +0530

    #include<stdio.h> int main() {     int v[2004],k,r,l,d,n=0;     scanf("%d %d",&l,&d);     for(r=0;r<l;r++)     {         scanf("%d",&v[r]);     }     for(r=0;r<l;r++)     {         if(v[r]%d==0)         {             n++;         }     }     printf("%d",n); }

---
+++ Count_elements_divisible_by_k.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int v[2004],k,r,l,d,n=0;
+    scanf("%d %d",&l,&d);
+    for(r=0;r<l;r++)
+    {
+        scanf("%d",&v[r]);
+    }
+    for(r=0;r<l;r++)
+    {
+        if(v[r]%d==0)
+        {
+            n++;
+        }
+    }
+    printf("%d",n);
+}
