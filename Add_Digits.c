3386977a6fb9a28cde441ad51163d4c80357f622
shiva <97592150+shiva8504@users.noreply.github.com>
2022-07-02 10:53:54 +0530

    #include<stdio.h> int happy(int x) { 	int sum=0,r; 	while (1>0) 	{ 		sum=0; 		while (x>0) 		{ 			r=x%10; 			sum+=r; 			x=x/10; 		} 		if (sum>9) 		{ 			x=sum; 			continue; 		} 		else 		{ 			break; 		} 	} 	return sum; } int main() { 	int a,res; 	scanf("%d",&a); 	res=happy(a); 	printf("%d",res); 	return 0; }

--- Add_Digits.c
+++ Add_Digits.c
@@ -1,18 +1,33 @@
 #include<stdio.h>
+int happy(int x)
+{
+	int sum=0,r;
+	while (1>0)
+	{
+		sum=0;
+		while (x>0)
+		{
+			r=x%10;
+			sum+=r;
+			x=x/10;
+		}
+		if (sum>9)
+		{
+			x=sum;
+			continue;
+		}
+		else
+		{
+			break;
+		}
+	}
+	return sum;
+}
 int main()
 {
-    int n,d,sum=0;
-    scanf("%d",&n);
-    while(n)
-    {
-        d=n%10;
-        sum+=d;
-        n=n/10;
-        if(n==0 && sum>9)
-        {
-            n=sum;
-            sum=0;
-        }
-    }
-    printf("%d",sum);
+	int a,res;
+	scanf("%d",&a);
+	res=happy(a);
+	printf("%d",res);
+	return 0;
 }
